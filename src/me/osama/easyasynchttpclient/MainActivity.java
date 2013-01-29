f60a83846117f7ccc49f9b8af607c1a14445ec34
Osama Rao <osama.rao@hotmail.com>
2013-01-30 00:16:49 +0500

    last commit before new files

--- src/me/osama/easyasynchttpclient/MainActivity.java
+++ src/me/osama/easyasynchttpclient/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this should change bin
+        
+        System.out.println("yea!");
     }
 
     @Override
