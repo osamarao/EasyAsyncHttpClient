b9e353555dc7036c9ec48bed607f85378e17126f
Osama Rao <osama.rao@hotmail.com>
2013-01-30 01:48:53 +0500

    Added java class for HttpGet request

--- src/me/osama/easyasynchttpclient/MainActivity.java
+++ src/me/osama/easyasynchttpclient/MainActivity.java
@@ -1,18 +1,80 @@
 package me.osama.easyasynchttpclient;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private Handler handler;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this should change bin
         
         System.out.println("yea!");
+        ArrayList<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
+//        parameters.add(new BasicNameValuePair("name", "osama"));
+//        parameters.add(new BasicNameValuePair("name", "tooba"));
+//        parameters.add(new BasicNameValuePair("name", "daania"));
+//        parameters.add(new BasicNameValuePair("name", "minahil"));
+        
+        handler = new Handler(new Callback() {
+			
+			@Override
+			public boolean handleMessage(Message msg) {
+				Bundle data = msg.getData();
+				if (data.getInt("error") == 1){
+					//XXX ERROR HANDLING
+					
+					
+					return false;
+				} else if (data.getInt("error") == 0){
+					//XXX NORMAL WORK
+					// use activity.runOnUIThread() for UI calls
+					
+					final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+					.setMessage(data.getString("response"))
+					.setPositiveButton("OK", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					}).create();
+					
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							alertDialog.show();
+						}
+					});
+					return false;
+				}
+				
+				return false;
+			}
+		});
+        String url = "http://petarosoft.com/android/samplejson";
+        
+        AsyncHttpGetClient asyncHttpGetClient = new AsyncHttpGetClient(MainActivity.this, url, parameters , handler);
+        asyncHttpGetClient.execute("");
+        
+        
+        
     }
 
     @Override
